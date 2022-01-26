4ceb2dde778e27ddb55ef0b72b31c6116e145499
ivan <Mr.Chudvan@yandex.ru>
2022-01-26 10:25:17 +0300

    in Data_4 add groupby foo + diff and group_by (min, max)

--- NeuralNetwork/tools.py
+++ NeuralNetwork/tools.py
@@ -24,3 +24,50 @@ create table {name_db} (
     connection.commit()
     return connection
 
+def groupby(df, by=None):
+    pa_fields =     [
+    'Valence',
+    'Arousal'
+    ]
+    seven_fields = [
+        'Neutral', 
+        'Happy', 
+        'Sad', 
+        'Angry', 
+        'Surprised', 
+        'Scared', 
+        'Disgusted'
+    ]
+    
+    if by is None:
+        by = pa_fields
+        
+    df_copy = df[seven_fields + pa_fields].copy()
+    
+    for field in pa_fields:
+        df_copy[field] = df_copy[field].apply(lambda x: round(float(x), 2))
+    for field in seven_fields:
+        df_copy[field] = df_copy[field].apply(lambda x: float(x))
+    
+    df_copy.index = df['Index_']
+    
+    groupby_fields_sorted = list(sorted(df_copy.groupby(by), key=lambda x: -len(x[1])))
+    for group in groupby_fields_sorted:
+        for field in seven_fields:
+            group[1][field] = round(group[1][field].mean(), 2)
+            
+    df_train = pd.DataFrame()
+    
+    for group in groupby_fields_sorted:
+        len_group = len(group[1])
+        ln_ = np.log10(len_group)
+        rand_set = set()
+        for _ in range(int(round(ln_, 0)) + 1):
+            i = random.randint(0, len_group - 1)
+            while i in rand_set:
+                i = random.randint(0, len_group - 1)
+            rand_set.add(i)
+            df_train = pd.concat([df_train, group[1].iloc[i:i + 1]], axis=0)
+    
+    return df_train
+
